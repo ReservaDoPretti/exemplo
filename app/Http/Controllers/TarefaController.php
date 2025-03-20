b178a7a5cba561e2534c6e7d13e77d26557d25b1
ReservaDoPretti <reservadopretti@outlook.com>
2025-03-20 09:47:56 -0300

    adicionado funcionalidade no controller

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -2,23 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tarefa;
 use Illuminate\Http\Request;
 
 class TarefaController extends Controller
 {
+
+    public function index (){
+        return Tarefa::all();
+    }
+
+
     public function store (Request $request){
-        
+        $tarefa =Tarefa::create($request->all());
+        return response()->json($tarefa,201);
     }
 
     public function show (string $id){
-        
+        return Tarefa::find($id);
     }
 
     public function update (Request $request, string $id){
-
+        $tarefa = Tarefa::find($id);
+        $tarefa->update($request->all());
+        return response()->json($tarefa, 200);
     }
 
     public function destroy (string $id){
-
+        $tarefa = Tarefa::find($id)->delete();
+        return response()->json(null, 204);
     }
 }
